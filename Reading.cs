59c75f2634f41853c977065caa18a97a3bffe44e
iOut1 <63367424+iOut1@users.noreply.github.com>
2022-02-08 13:51:28 -0800

    Created Program for Reading.

---
+++ Reading.cs
@@ -0,0 +1,13 @@
+using IronOcr;
+using System;
+
+namespace Reading
+{
+  class Read
+  {
+    public void Main(string[] args)
+    {
+      Console.ReadLine("Enter Location of Image: \n");    
+    }
+  }
+}
