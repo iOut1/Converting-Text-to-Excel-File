41729fd11969596b53874404a71b99bd3139a61e
David Benshachar <63367424+iOut1@users.noreply.github.com>
2022-02-17 14:33:09 -0800

    Added comments.

--- Reading.cs
+++ Reading.cs
@@ -7,12 +7,16 @@ namespace Reading
   {
     static void Main(string[] args)
     {
+      //Input for file location//
       Console.WriteLine("Enter file name: \n");
       string File = Console.ReadLine();
-
+      
+      //Reads the file and turns it into text//
       var Ocr = new IronTesseract();
       Ocr.Language = OcrLanguage.English;
       var Result = IronTesseract().Read(File).Text;
+      
+      //Returns the text//
       return Result
     }
   }
